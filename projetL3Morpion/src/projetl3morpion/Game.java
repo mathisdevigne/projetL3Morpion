fa507a3f7efa0488f941e33d6556b7aabe45ceaf
Steve <steve.fetiveau@gmail.com>
2022-10-18 00:04:02 +0200

    Version joueur humain du morpion

--- projetL3Morpion/src/projetl3morpion/Game.java
+++ projetL3Morpion/src/projetl3morpion/Game.java
@@ -50,7 +50,61 @@ public class Game {
     }
     
     
-    public void playOneRound(){
+    public boolean playOneRound(int nbJoueur) throws IOException{
+        int tab[] = new int[2];
+        // Si la case est libre alors on place sinon on redemande
+        do{
+            
+        tab[0] = getIntInput("Joueur "+ nbJoueur +", à vous de jouer: ");
+        tab[1] = getIntInput("");
+        
+        }while(!(" ".equals(this.getGameboard().getBoxBoard(tab[0], tab[1]).getValue()) && tab[0] >= 0 && tab[1] >= 0 && tab[0] < this.getGameboard().getHeight() && tab[1] < this.getGameboard().getWidth()));
+        
+        this.insertValue(nbJoueur, tab[0], tab[1]);
+        this.getGameboard().print();
+        return this.hasWin(tab[0], tab[1]);
+    }
+    
+    public void insertValue(int nbJoueur, int x, int y){
+        if(nbJoueur == 1){
+            this.getGameboard().getBoxBoard(x, y).setValue("X");
+        }
+        else{
+            this.getGameboard().getBoxBoard(x, y).setValue("O");
+        }
+    }
+    
+    //Va regarder le dernier coup jouer et renvoyé true si ce coup permet de gagner
+    public boolean hasWin(int x, int y){
+        boolean hasWin = false;
+        Box[][] quintuplets = this.getGameboard().getQuintuplets(x, y);
+        for(int i = 0; i < this.getGameboard().getNbQuintuplets(x, y) ; i++ ){
+            if("X".equals(quintuplets[i][0].getValue()) && "X".equals(quintuplets[i][1].getValue()) && "X".equals(quintuplets[i][2].getValue()) && "X".equals(quintuplets[i][3].getValue()) && "X".equals(quintuplets[i][4].getValue())){
+                System.out.println("Joueur 1 a gagné");
+                hasWin = true;
+            }
+            else if("O".equals(quintuplets[i][0].getValue()) && "O".equals(quintuplets[i][1].getValue()) && "O".equals(quintuplets[i][2].getValue()) && "O".equals(quintuplets[i][3].getValue()) && "O".equals(quintuplets[i][4].getValue())){
+                System.out.println("Joueur 2 a gagné");
+                hasWin = true;
+            }
+        }
+        return hasWin;
+    }
+    
+    public void play() throws IOException{
+        boolean ended = false;
+        int joueur = 1;
+        this.getGameboard().print();
+        while(!(ended)){
+            ended = playOneRound(joueur);
+            if(joueur == 1){
+                joueur = 2;
+            }
+            else{
+                joueur = 1;
+            }
+        }
+        
         
     }
 }
